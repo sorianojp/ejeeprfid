f6ee104ae3a10165c95a59e40555b4b32f97f86a
John Paul Soriano <jppromax@jpthekingmacbookpro.local>
2025-08-14 14:34:34 +0800

    update

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('students.index')">
+                    <x-nav-link :href="route('students.index')" :active="request()->routeIs('students.index')">
                         {{ __('Register Student') }}
                     </x-nav-link>
                 </div>
